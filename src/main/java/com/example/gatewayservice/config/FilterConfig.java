d204b33c525c2f3a1f1e91322278a0bcd4d5e2b7
Develing1991 <completed0728@gmail.com>
2023-12-04 02:25:47 +0900

    comment: add filter config java register CustomFilter

--- src/main/java/com/example/gatewayservice/config/FilterConfig.java
+++ src/main/java/com/example/gatewayservice/config/FilterConfig.java
@@ -1,19 +1,29 @@
 package com.example.gatewayservice.config;
 
+import com.example.gatewayservice.filter.CustomFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// java로 커스텀 필터 등록 방법 yaml이랑 택 1
+/*
+
 @Configuration
+@RequiredArgsConstructor
 public class FilterConfig {
+
+    private final CustomFilter customFilter; // java로 커스텀 필터 등록 방법
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
         return builder.routes()
                 .route(r ->
                         r.path("/first-service/**")
                         .filters( f -> f.addRequestHeader("first-request", "first-request-header")
-                                        .addResponseHeader("first-response", "first-response-header"))
+                                        .addResponseHeader("first-response", "first-response-header")
+                                .filter(customFilter.apply(customFilter.newConfig())) // java로 커스텀 필터 등록 방법
+                        )
                         .uri("http://localhost:8081")
                 )
                 .route(r ->
@@ -25,3 +35,4 @@ public class FilterConfig {
                 .build();
     }
 }
+*/
