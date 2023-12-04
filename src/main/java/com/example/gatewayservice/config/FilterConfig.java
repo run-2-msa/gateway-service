5d2d71d5ee070da7f757b58acdb7732cba81a4a6
Develing1991 <completed0728@gmail.com>
2023-12-04 13:09:27 +0900

    feature: add gateway java code, load balancing, filter

--- src/main/java/com/example/gatewayservice/config/FilterConfig.java
+++ src/main/java/com/example/gatewayservice/config/FilterConfig.java
@@ -1,38 +1,40 @@
 package com.example.gatewayservice.config;
 
 import com.example.gatewayservice.filter.CustomFilter;
-import lombok.RequiredArgsConstructor;
+import com.example.gatewayservice.filter.LoggingFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// java로 커스텀 필터 등록 방법 yaml이랑 택 1
-/*
+// java로 필터 및 lb 작성
 
 @Configuration
-@RequiredArgsConstructor
 public class FilterConfig {
 
-    private final CustomFilter customFilter; // java로 커스텀 필터 등록 방법
     @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder, CustomFilter customFilter, LoggingFilter loggingFilter){
+
         return builder.routes()
                 .route(r ->
                         r.path("/first-service/**")
                         .filters( f -> f.addRequestHeader("first-request", "first-request-header")
                                         .addResponseHeader("first-response", "first-response-header")
-                                .filter(customFilter.apply(customFilter.newConfig())) // java로 커스텀 필터 등록 방법
+                                        .filters(customFilter.apply(new CustomFilter.Config()),
+                                                loggingFilter.apply(new LoggingFilter.Config())
+                                                )
+                                        // .rewritePath()
                         )
-                        .uri("http://localhost:8081")
+                        .uri("lb://FIRST-SERVICE")
                 )
                 .route(r ->
                         r.path("/second-service/**")
                         .filters( f -> f.addRequestHeader("second-request", "second-request-header")
-                                        .addResponseHeader("second-response", "second-response-header"))
-                        .uri("http://localhost:8082")
+                                        .addResponseHeader("second-response", "second-response-header")
+                                        .filter(customFilter.apply(customFilter.newConfig()))
+                        )
+                        .uri("lb://SECOND-SERVICE")
                 )
                 .build();
     }
 }
-*/
